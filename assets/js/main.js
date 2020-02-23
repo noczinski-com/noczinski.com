80bf03fc8686707a68cbd52377097de47f4ca1c8
Gregor Noczinski <gregor@noczinski.eu>
2020-02-23 17:40:09 +0100

    Optimize resource loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,25 @@ function displayWindowSize() {
     }
 }
 
-
 try {
     window.addEventListener("resize", displayWindowSize);
     window.addEventListener("load", displayWindowSize);
+
+    async function supportsWebp() {
+        if (!self.createImageBitmap) return false;
+
+        const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+        const blob = await fetch(webpData).then(r => r.blob());
+        return createImageBitmap(blob).then(() => true, () => false);
+    }
+
+    (async () => {
+        if(await supportsWebp()) {
+            document.body.classList.add('webp-supported');
+        } else {
+            document.body.classList.add('webp-not-supported');
+        }
+    })();
 } catch (e) {
     if (window.debug) {
         throw e;
