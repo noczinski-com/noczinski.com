8f23ab9657f179285acef8ec33a952ca27d08051
blaubaer <blaubaer@users.noreply.github.com>
2020-03-01 15:33:53 +0000

    Automated deployment: Sun Mar  1 15:33:52 UTC 2020 5becc71c5032af32b05ce7b1acde5dcd611d0311

---
+++ js/bundle.min.1756a0d7bfe91236967bc6cd7aa37921726050415cfde2212288ac315ceac9f3.js
@@ -0,0 +1,7 @@
+function unsafeExecution(what,onError){try{return what();}catch(e){if(window.console&&window.console.debug){console.debug('Operation was not successful.',what,e);}
+if(onError){return onError();}}}
+function displayWindowSize(){unsafeExecution(function(){var elements=Array.from(document.getElementsByTagName('html')).concat(Array.from(document.getElementsByTagName('body')));var windowHeight=document.documentElement.clientHeight;var bodyHeight=document.body.clientHeight;for(var i=0;i<elements.length;i++){if(elements[i].style!==undefined){if(windowHeight>bodyHeight){elements[i].style.height='100%';}else{elements[i].style.height='auto';}}}})}
+function supportsWebp(callback){unsafeExecution(function(){if(!window.createImageBitmap){callback(false);return;}
+var data='data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';fetch(data).then(function(response){return response.blob();}).then(function(blob){createImageBitmap(blob).then(function(){callback(true);},function(){callback(false);});});})}
+function webpCheck(){supportsWebp(function(supported){unsafeExecution(()=>{var elements=document.getElementsByTagName('body');for(var i=0;i<elements.length;i++){elements.item(i).setAttribute('data-webp-supported',supported);}})});}
+window.addEventListener("resize",displayWindowSize);window.addEventListener("load",displayWindowSize);window.addEventListener("load",webpCheck);
